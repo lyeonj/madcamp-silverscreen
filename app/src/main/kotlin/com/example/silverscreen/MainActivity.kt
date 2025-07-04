d4c04261ca0c188c2284b98a48061eed05856f8c
Yeonjae Lee <lyeonj@sookmyung.ac.kr>
2025-07-05 03:54:36 +0900

    chore: 불필요한 코드 삭제

--- app/src/main/kotlin/com/example/silverscreen/MainActivity.kt
+++ app/src/main/kotlin/com/example/silverscreen/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.silverscreen.databinding.ActivityMainBinding
 
@@ -20,15 +18,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_wish, R.id.navigation_seen, R.id.navigation_map
-            )
-        )
 
         navView.setupWithNavController(navController)
     }
