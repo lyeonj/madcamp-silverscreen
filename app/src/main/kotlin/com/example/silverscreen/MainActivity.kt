73200893df4f93994f46e79958bf964de7d3c67b
Changmin Lee <107976623+chngmn@users.noreply.github.com>
2025-07-07 11:11:53 +0900

    feat: Splash Screen 초기 구현

--- app/src/main/kotlin/com/example/silverscreen/MainActivity.kt
+++ app/src/main/kotlin/com/example/silverscreen/MainActivity.kt
@@ -6,12 +6,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.silverscreen.databinding.ActivityMainBinding
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // 1) SplashScreen 초기화
+        val splash = installSplashScreen()
+
+        // 2) 앱 런칭 시점 기록
+        val start = System.currentTimeMillis()
+        // 3) 전체 2000ms 동안 유지하도록 조건 지정
+        splash.setKeepOnScreenCondition {
+            System.currentTimeMillis() - start < 2000L
+        }
+
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
